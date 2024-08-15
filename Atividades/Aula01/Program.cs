7dafd2e7b21f0eed859886e6053e5cbf41848684
Thiago Wurster Balbinot <255285@unoesc.lan>
2024-08-14 22:07:10 -0300

    Primeiro Projeto .net

---
+++ Atividades/Aula01/Program.cs
@@ -0,0 +1,17 @@
+﻿//Comentário de uma linha
+
+/*
+    Comentário de 
+    Múltiplas Linhas
+*/
+
+//Declarando Variáveis
+int number;
+//Atribuindo Valor
+number = 10;
+//Declarando Constante
+const int DAYS_IN_WEEK = 7;
+//Imprimindo String Concatenada
+Console.WriteLine($"A Semana tem {DAYS_IN_WEEK} dias.");
+
+

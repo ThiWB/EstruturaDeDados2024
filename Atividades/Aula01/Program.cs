4f90ac3b8dd823fbb15888fd075c708116e2d4bf
Thiago Wurster Balbinot <255285@unoesc.lan>
2024-08-21 21:51:28 -0300

    Aula Classe, Objeto e Num

--- Atividades/Aula01/Program.cs
+++ Atividades/Aula01/Program.cs
@@ -6,12 +6,28 @@
 */
 
 //Declarando Variáveis
+using Aula01;
+
 int number;
 //Atribuindo Valor
 number = 10;
+
 //Declarando Constante
 const int DAYS_IN_WEEK = 7;
+
 //Imprimindo String Concatenada
 Console.WriteLine($"A Semana tem {DAYS_IN_WEEK} dias.");
+
+// new TipoEnumerador() -> invoca o método construtor do objeto
+// Toda classe tem o seu construtor padrão implícito sem argumentos ou parâmetros
+// É possivel sobescrever este método especificando argumentos
+// Todavia, se fizermos, perdemos o original implícito e precisaremos defini-lo explicitamente
+TipoEnumerador tipoEnum = new TipoEnumerador();
+
+TipoEnumerador.Language enumEnglish =
+ tipoEnum.GetLanguageEnum("inglês");
+
+Console.WriteLine( $"O enum de english é {enumEnglish}" );
+
 
 

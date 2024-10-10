f6bdab098a6c2b7ee214255f0bd8762634c5e405
Thiago Wurster Balbinot <255285@unoesc.lan>
2024-10-09 21:53:01 -0300

    BubbleSort

---
+++ Atividades/AlgoritmosOrdenacao/BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using BubbleSort;
+
+int[] inteiros = {0, 12, 7, 1, 2, 8};
+
+Console.WriteLine("Números sem Filtrar");
+BubbleSort.BubbleSort.Print(inteiros);
+
+BubbleSort.BubbleSort.Sort<int>(inteiros);
+
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.WriteLine("Números Filtrados com BubbleSort");
+BubbleSort.BubbleSort.Print(inteiros);
+
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.WriteLine("Teste Char");
+
+char[] chars = ['x', 'z', 'd', 'c', 'a'];
+BubbleSort.BubbleSort.Print(chars);
+BubbleSort.BubbleSort.Sort(chars);
+Console.WriteLine("");
+BubbleSort.BubbleSort.Print(chars);
